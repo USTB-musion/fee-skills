0e9a582b578d9a2184e2f975d22a8c32df315f59
JeeLee <18810996718@163.com>
2017-08-20 10:52:39 +0800

    add something

---
+++ algorithm/sort.js
@@ -0,0 +1,16 @@
+/*冒泡排序算法*/
+var examplearr=[8,94,15,88,55,76,21,39];
+function sortarr(arr){
+    for(i=0;i<arr.length-1;i++){
+        for(j=0;j<arr.length-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                var temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    return arr;
+}
+sortarr(examplearr);
+console.log(examplearr);           //[ 8, 15, 21, 39, 55, 76, 88, 94 ]
