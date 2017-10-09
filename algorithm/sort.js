2084fc5f659ff9f3d8fe5730d1756b63fbae5191
JeeLee <18810996718@163.com>
2017-10-09 19:21:45 +0800

    add insertSort

--- algorithm/sort.js
+++ algorithm/sort.js
@@ -14,3 +14,48 @@ function sortarr(arr){
 }
 sortarr(examplearr);
 console.log(examplearr);           //[ 8, 15, 21, 39, 55, 76, 88, 94 ]
+
+
+/*选择排序*/
+var sortArr = [1,23,4,56,41,6,3];
+
+function sort(arr) {
+    var len = arr.length;
+    var minIndex, temp;
+    for (var i = 0; i < len - 1; i++) {
+        minIndex = i;
+        for (var j = i + 1; j < len; j++) {
+            if (arr[j] < arr[minIndex]) {     
+                minIndex = j;            
+            }
+            temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+       
+    }
+    return arr;
+}
+
+sort(sortArr);
+console.log(sortArr);       //1, 3, 6, 4, 23, 41, 56
+
+
+
+//插入排序
+    function insertSort(arr){
+        var len = arr.length;
+        for(var i = 1;i<len;i++) {
+            var key = arr[i];
+            var j = i - 1;
+            while(j >=0 && arr[j] > key) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+        return arr;
+    }
+
+    var exmaplearr = [12,23,655,34,676,6764];
+    console.log(insertSort(exmaplearr));
